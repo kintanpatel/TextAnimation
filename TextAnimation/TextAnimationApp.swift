6238948e9a7f16536ac71c347dfa491306cabde5
Kintan Patel <kintanpatel4393@gmail.com>
2024-05-01 13:03:58 +0530

    Initial Commit

---
+++ TextAnimation/TextAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextAnimationApp.swift
+//  TextAnimation
+//
+//  Created by kintan on 01/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TextAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
