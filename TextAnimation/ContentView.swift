8a686b0c7af1b60cb6f65c8e867cb67ecb4053df
Kintan Patel <kintanpatel4393@gmail.com>
2024-05-01 13:41:56 +0530

    - Text animation added

--- TextAnimation/ContentView.swift
+++ TextAnimation/ContentView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let letters = Array("Hello, Kintan")
+    @State private var dragAmount = CGSize.zero
+    @State private var isEnable = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        HStack(spacing : 0) {
+            ForEach(0..<letters.count,id: \.self){ num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .background(isEnable ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(.default.delay(Double(num)/20), value: dragAmount)
+            }
+        }.gesture(DragGesture().onChanged{
+            dragAmount = $0.translation
+        }.onEnded({ _ in
+            dragAmount = .zero
+            isEnable.toggle()
+        }))
+        
     }
 }
 
